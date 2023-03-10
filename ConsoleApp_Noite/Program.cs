1cf3ab178995a649c773a31be859548029134093
yTheuZn <matheus.ro.oli@outlook.com>
2023-03-10 19:46:21 -0300

    Criação da classe produto, alteração da classe program.

--- ConsoleApp_Noite/Program.cs
+++ ConsoleApp_Noite/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp_Noite;
+
 Console.WriteLine("Hello, World!");
+
+Produto p1 = new Produto(1,10.90M, "Cervejinha");
+
+Console.WriteLine("");
